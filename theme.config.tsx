020dc29d79bcf7683055f4d1d48f0231dc301237
Nathan Clevenger <nateclev@gmail.com>
2023-03-02 17:45:53 -0600

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>∵ GraphDL</span>,
+  logo: <span>🜉 GraphDL</span>,
   project: {
     link: 'https://github.com/graphdl/graphdl',
   },
